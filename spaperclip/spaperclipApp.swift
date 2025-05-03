7657616a499ea40bf4a1631fe0936c2a1319dc43
Scott Opell <me@scottopell.com>
2025-05-03 16:48:54 -0400

    Initial Commit

---
+++ spaperclip/spaperclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaperclipApp.swift
+//  spaperclip
+//
+//  Created by Scott Opell on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct spaperclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ spaperclipTests/spaperclipTests.swift
@@ -0,0 +1,17 @@
+//
+//  spaperclipTests.swift
+//  spaperclipTests
+//
+//  Created by Scott Opell on 5/3/25.
+//
+
+import Testing
+@testable import spaperclip
+
+struct spaperclipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
